e33d7e70b36b2d0226fee99209d874c48450c5db
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-10 00:26:09 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+    let expression = expr.split(' ');
+    let sign = [];
+
+    expression.forEach(element => {
+        if (isNaN(element)) {
+            sign.push(element);
+            const index = expression.indexOf(element);
+            expression.splice(index, 1);
+        }
+    });
+    console.log(expression);
+    console.log(sign);
 }
 
 module.exports = {
