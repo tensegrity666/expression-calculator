82877cfa48302d731f47f0a5e0178d69e6f0c217
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-12 23:27:25 +0300

    feat: implement of  simple calculation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 function expressionCalculator(expr) {
     let expression = expr.split(' ');
     let sign = [];
+    let result = 0;
 
     expression.forEach(element => {
         if (isNaN(element)) {
-            sign.push(element);
+            sign = element;
             const index = expression.indexOf(element);
             expression.splice(index, 1);
         }
     });
     console.log(expression);
     console.log(sign);
+
+    if (sign === '*') {
+        result = +expression[0] * +expression[1];
+    }
+    if (sign === '/') {
+        if (expression[1] === '0') throw Error('TypeError: Division by zero.');
+        result = +expression[0] / +expression[1];
+    }
+    if (sign === '+') {
+        result = +expression[0] + +expression[1];
+    }
+    if (sign === '-') {
+        result = +expression[0] - +expression[1];
+    }
+
+    return result;
 }
 
 module.exports = {
