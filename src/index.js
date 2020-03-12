7c687474a4a4e49149ba81d627df9d5563c9b8e8
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-13 02:31:17 +0300

    feat: add check for brackets pairs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 function expressionCalculator(expr) {
-    expr = expr.replace(/\s/g, '').replace(/(\*|\/|\+|\-)/g, ' $& ');
+    expr = expr.replace(/\s/g, '').replace(/(\*|\/|\+|\-|\(|\))/g, ' $& ');
     let expression = expr.split(' ');
+    let bracketsOpening = 0;
+    let bracketsClosing = 0;
 
     for (i = 0; i < expression.length; i++) {
+        if (expression[i] === '(') {
+            bracketsOpening++;
+        }
+        if (expression[i] === ')') {
+            bracketsClosing--;
+        }
+        if (bracketsOpening !== bracketsClosing) {
+            throw Error('ExpressionError: Brackets must be paired');
+        };
+
         if (expression[i] === '*') {
             expression[i] = +expression[i - 1] * +expression[i + 1];
             expression.splice(i - 1, 1);
